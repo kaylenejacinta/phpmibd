ec7dd19b6e2c68e2d36f2465a0d5c454a5113be0
kaylenejacinta <108456739+kaylenejacinta@users.noreply.github.com>
2026-06-08 21:33:33 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,15 +1,9 @@
 <?php
-$serverName = "localhost, 1433";
-$database   = "FRS_db";
-$user       = "Project";
-$password   = "project123";
+$serverName = "LAPTOP-H2UFRUQF\SQLEXPRESS"; 
+$database = "TUBES_FRS";
 
 try {
-    $pdo = new PDO(
-        "sqlsrv:Server=$serverName;Database=$database;TrustServerCertificate=1",
-        $user,
-        $password
-    );
+    $pdo = new PDO("sqlsrv:Server=LAPTOP-H2UFRUQF\SQLEXPRESS;Database=TUBES_FRS;TrustServerCertificate=true");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
@@ -17,3 +11,5 @@ try {
     die(json_encode(["error" => $e->getMessage()]));
 }
 ?>
+
+
