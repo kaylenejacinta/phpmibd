312985f9d1f02f5938cf1bfd158c2ac471cbdf63
kaylenejacinta <108456739+kaylenejacinta@users.noreply.github.com>
2026-06-09 10:02:46 +0700

    Update matakuliah.php

--- matakuliah.php
+++ matakuliah.php
@@ -2,7 +2,6 @@
 // get_matkul.php
 // Return semua Mata Kuliah yang di join dgn Jadwal and Dosen
 // Digunakan oleh pengisian_frs.html, jadwal_kuliah.html, dashboard.html (mahasiswa)
-
 header("Content-Type: application/json");
 require 'db.php';
 
@@ -12,22 +11,17 @@ try {
             mk.Kode_Matkul,
             mk.Nama_Matkul,
             mk.SKS,
-            mk.Id_Semester,
             j.Id_Jadwal,
             j.Hari,
-            CONVERT(varchar, j.Jam_Mulai, 108)    AS Jam_Mulai,
-            CONVERT(varchar, j.Jam_Selesai, 108)  AS Jam_Selesai,
+            CONVERT(varchar, j.Jam_Mulai, 108)   AS jam_mulai,
+            CONVERT(varchar, j.Jam_Selesai, 108) AS jam_selesai,
             j.Ruangan,
-            d.Nama AS Nama_Dosen,
-            s.Tahun_Akademik,
-            s.Periode
+            d.Nama AS Nama_Dosen
         FROM Mata_Kuliah mk
-        LEFT JOIN Jadwal_Matkul j  ON mk.Kode_Matkul = j.Kode_Matkul
-        LEFT JOIN Dosen d          ON j.Id_Dosen = d.Id_Dosen
-        LEFT JOIN Semester s       ON mk.Id_Semester = s.Id_Semester
+        LEFT JOIN Jadwal_Matkul j ON mk.Kode_Matkul = j.Kode_Matkul
+        LEFT JOIN Dosen d         ON j.Id_Dosen = d.Id_Dosen
         ORDER BY mk.Kode_Matkul
     ");
-
     echo json_encode($stmt->fetchAll());
 } catch (PDOException $e) {
     http_response_code(500);
